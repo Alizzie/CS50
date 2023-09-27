88ff48e665acd480591df6bdc6c1fe3c73de5ff8
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-28 01:04:01 +0200

    professor add multiple tries

--- week4/professor.py
+++ week4/professor.py
@@ -3,17 +3,26 @@ import random
 
 def main():
     level = get_level()
+    solved_problems = 0
 
     for _ in range(10):
         x = generate_integer(level)
         y = generate_integer(level)
         result = x + y
-        print(result)
-        user_result = int(input(f"{x} + {y} = "))
 
-        if result != user_result:
+        user_tries = 0
+        while user_tries < 3:
+            user_result = int(input(f"{x} + {y} = "))
+            if result == user_result:
+                solved_problems += 1
+                break
             print("EEE")
-
+            user_tries += 1
+        
+        if user_tries == 3:
+            print(f"Result: {result}")
+        
+    print(f"Solved: {solved_problems}")
 
 def get_level():
     while True:
