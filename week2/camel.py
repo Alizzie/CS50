01f8e3fabdec26c0df5c850142c7376973fadb4d
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-27 17:07:03 +0200

    camel added convertion to snakecase

--- week2/camel.py
+++ week2/camel.py
@@ -1,4 +1,8 @@
 user_input = input("camelCase: ")
 
+print(user_input[0], end="")
 for character in user_input[1:]:
-    print(character)
+    if character.isupper():
+        print(f"_{str.lower(character)}", end="")
+    else:
+        print(character, end="")
