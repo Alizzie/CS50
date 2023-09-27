e30cf834f8b451ca005af2b5c0453bd3a8f8d0cd
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-28 00:28:13 +0200

    game add guessing

--- week4/game.py
+++ week4/game.py
@@ -3,6 +3,21 @@ import random
 def main():
     user_level = getLevelInput()
     target_number = random.randint(1, user_level)
+    print(target_number)
+    
+    while True:
+        try:
+            guess = int(input("Guess: "))
+
+            if guess > target_number:
+                print("Too large")
+            elif guess < target_number:
+                print("Too small")
+            else:
+                print("Just right!")
+                break
+        except:
+            pass
 
 def getLevelInput():
     while True:
