202eb9a6377d3dd7f0ec7f7ba54b188dae335068
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-28 00:57:43 +0200

    professor add generating integer and math problems

--- week4/professor.py
+++ week4/professor.py
@@ -3,7 +3,16 @@ import random
 
 def main():
     level = get_level()
-    print(level)
+
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        result = x + y
+        print(result)
+        user_result = int(input(f"{x} + {y} = "))
+
+        if result != user_result:
+            print("EEE")
 
 
 def get_level():
@@ -12,12 +21,16 @@ def get_level():
             level = int(input("Level: "))
             if 0 < level < 4:
                 return level
-        except:
+            else:
+                raise ValueError()
+        except ValueError:
             pass
 
 
 def generate_integer(level):
-    ...
+    start = 10**(level-1)
+    end = (10**(level)) - 1
+    return random.randint(start, end)
 
 
 if __name__ == "__main__":
