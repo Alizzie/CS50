cd42531254f4872946713fa85237c02b4e92da2c
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-27 19:32:33 +0200

    grocery added list containing all user inputs

--- week3/grocery.py
+++ week3/grocery.py
@@ -1,1 +1,14 @@
-user_input = input()
+def main():
+    grocery_list = []
+
+    while True:
+        try:
+            user_input = input()
+        except EOFError:
+            print("\n")
+            break
+        else:
+            grocery_list.append(user_input)
+            print(grocery_list)
+
+main()
