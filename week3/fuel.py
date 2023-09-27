8e56b386d045ee4e757335b29ac789fe2e3bf184
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-27 19:00:59 +0200

    fuel problem added error cases

--- week3/fuel.py
+++ week3/fuel.py
@@ -1,13 +1,24 @@
 def main():
     percentage = getFractionPercentage()
-    print(f"{percentage}%")
+    print(percentage)
 
 def getFractionPercentage():
     while True:
         try:
             fraction = input("Fraction: ")
             x, y = fraction.split("/")
-            return int((int(x) / int(y)) * 100)
+
+            if x > y:
+                raise ValueError()
+
+            percentage = int((int(x) / int(y)) * 100)
+            if percentage >= 99:
+                return "F"
+            elif percentage <= 1:
+                return "E"
+            else:
+                return f"{percentage}%"
+
         except (ValueError, ZeroDivisionError):
             pass
 
