13bd8535e3e620b1f8a8c1a7827f8036554bd5fb
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-27 22:47:37 +0200

    figlet added all functionalities

--- week4/figlet.py
+++ week4/figlet.py
@@ -1,6 +1,23 @@
 from pyfiglet import Figlet
 import sys
+import random
 
 
 def main():
     figlet = Figlet()
+    fonts = figlet.getFonts()
+
+    if len(sys.argv) > 3: 
+        sys.exit()
+    elif len(sys.argv) == 1:
+        random_font = random.choice(fonts)
+    else: 
+        if (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in fonts:
+            figlet.setFont(font=sys.argv[2])
+        else:
+            sys.exit("Invalid usage")
+
+    user_input = input("Input: ")
+    print(figlet.renderText(user_input))
+
+main()
