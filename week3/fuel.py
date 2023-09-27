6526a6148bb6ff5292904fff747be70fd768c46b
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-09-27 18:45:23 +0200

    fuel week3 init, added error catching

--- week3/fuel.py
+++ week3/fuel.py
@@ -1,1 +1,14 @@
-user_input = input("Fraction: ")
+def main():
+    percentage = getFractionPercentage()
+    print(f"{percentage}%")
+
+def getFractionPercentage():
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            x, y = fraction.split("/")
+            return int((int(x) / int(y)) * 100)
+        except (ValueError, ZeroDivisionError):
+            pass
+
+main()
