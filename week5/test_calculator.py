fcc5e8a2ca2e48437db38184c3ef289095fba189
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-10-07 15:14:32 +0200

    test pytest code

--- week5/test_calculator.py
+++ week5/test_calculator.py
@@ -12,11 +12,27 @@ def main():
 #        print("3 squared was not 9")
 
 # Alternative with try-except but can become more burdensome
-def test_assert():
+#def test_assert():
+#    assert square(2) == 4
+#    assert square(3) == 9
+#    assert square(-2) == 4
+#    assert square(-3) == 9
+#    assert square(0) == 0
+
+# Pytest: F means Test failed
+# Pytest: . means passed
+# Separating into multiple functions more checkings. After finding one error in a function, test will move to another one
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+
+def test_zero():
     assert square(0) == 0
 
 if __name__ == "__main__":
