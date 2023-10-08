81a95d9381ff5044ccc7d96820b707d829fd8385
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-10-08 15:16:55 +0200

    notes week7 regular expressions

---
+++ week7/validate.py
@@ -0,0 +1,59 @@
+email = input("What's your email? ").strip()
+
+# @@ would be still valid
+#if "@" in email:
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+# @. would be valid
+#if "@" in email and "." in email:
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+# only with .edu and malan@.edu is valid
+#username, domain = email.split("@") 
+#if username and domain.endswith(".edu"):
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+# malan@havard is valid
+#username, domain = email.split("@")
+#if username and "." in domain:
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+# Python library re contains number of built-in functions
+# For validating user inputs against patterns
+
+# REGULAR EXPRESSIONS
+#.   any character except a new line
+#*   0 or more repetitions
+#+   1 or more repetitions
+#?   0 or 1 repetition
+#{m} m repetitions
+#{m,n} m-n repetitions
+# \ for escaping a character 
+#^   matches the start of the string
+#$   matches the end of the string or just before the newline at the end of the string
+#[]    set of characters
+#[^]   complementing the set
+#\d    decimal digit
+#\D    not a decimal digit
+#\s    whitespace characters
+#\S    not a whitespace character
+#\w    word character, as well as numbers and the underscore = [a-zA-Z0-9_]
+#\W    not a word character
+#A|B     either A or B
+#(...)   a group
+#(?:...) non-capturing version
+
+import re
+
+if re.search(r"^\w+@\w.+\.(com|edu|gov|net|org)$", email):
+    print("Valid")
+else:
+    print("Invalid")
